264083ffd1e0326b0731a7cefdfb6475cbbc7a3a
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2021-08-06 21:14:21 +0700

    version 0.1; implemented basic comment functionality

--- background.js
+++ background.js
@@ -1,15 +1,29 @@
-function isYoutubeUrlMatchFormat(url,output){
-    let parts = url.split('?');
-    if(parts.length!=2) return false;
-    if(parts[0]!='https://www.youtube.com/watch') return false;
-    let q = new URLSearchParams(parts[1]);
-    let arr = ['timeout','comment','row','col','sheetName'];
-    for(let key of arr){
-        if(q.get(key)==null) return false;
-        output[key] = q.get(key);
+// validates a URL generated from the GAS or a facebook comment URL
+function isFacebookUrlMatchFormat(url,output){
+    if(typeof(url)!='string') 
+        return false;
+    let pattern = /https:\/\/(www.)?facebook.com\/groups\/.+\/posts\/\d+\/(.+)$/;    // validates post and comment url with any parameters
+    let matches = url.match(pattern);
+    if(!matches || matches.length!=3) 
+        return false;     // ensures url is a facebook post and has query string
+    let urlParams = matches[2];
+    let q = new URLSearchParams(urlParams);
+    let paramKeys = [   // possible params
+                        'comment','row',    // params to make comment
+                        'comment_id','facebookct'];      // param to delete comment 
+    let o = {}
+    for(let key of paramKeys){
+        if(q.get(key)!=null) o[key] = q.get(key);
     }
+    output['params'] = o;
+    output['type'] = o['comment_id'] && o['facebookct']?'delete'
+                    :o['comment'] && o['row']?'comment'
+                    :null;
     return true;
 }
+/**
+ * Maintain working tabs to identify their message
+ */
 function getTabListInstance(){
     if(globalThis.tabList==undefined){
         globalThis.tabList = {};
@@ -24,25 +38,49 @@ function sendMessageContentScript(tabId, message){
  */
 function onTabUpdatedListener(tabId,changeInfo,tab){
     const TAG = 'onTabUpdatedListener';
-    console.log(`TabId ${tabId}, Url ${tab.url}: updated.`);
+    console.log(TAG,`TabId ${tabId}, Url ${tab.url}: updated.`);
     let output = {};
     let url = tab.url;
-    if(!isYoutubeUrlMatchFormat(url,output)) return;    // ensures this url matches our format
-    console.log(TAG,`TabId ${tabId}, Url ${tab.url}: Detected a matched Youtube URL. Parameters:`,output);
     let tabList = getTabListInstance();
-    if(tabList[tabId]!=undefined) return;  // ensures this tab id is not listed
-    tabList[tabId]=output;    // adds this tab id to list
+    if((!isFacebookUrlMatchFormat(url,output) || output['type']==null) && tabList[tabId]==undefined)   // checks if the tab either has invalid url or not tracked
+        return;
+    console.log(TAG,`TabId ${tabId}, Url ${tab.url}: Parameters:`,output);
+    if(tabList[tabId]==undefined){  // checks if this tabId is not tracked
+        tabList[tabId]=output;    // tracks tab id with parameters
+        let delim = '?';    // delimiter for type of "comment"
+        if(output.type==='delete'){
+            delim = '&';    // delimiter for type of "delete"
+        }
+        let navUrl = url.substring(0,url.indexOf(delim));   // gets original url
+        console.log(TAG,`Navigating ${tabId} from ${url} to ${navUrl}..`)
+        chrome.tabs.update(tabId,{url:navUrl}); // navigates the tab to the same url without query string
+        return;
+    }
     chrome.scripting.executeScript({    // inject content.js script
         target:{tabId},
         files: ['content.js']
     });
-    console.log(`TabId ${tabId}, Url ${tab.url}: script injected.`);
+    console.log(TAG,`TabId ${tabId}, Url ${tab.url}: script injected.`);
 }
+/**
+ * Ensures the request contains required keys to generate the payload as in `generatePayload` function
+ */
+function isMessageMatchFormat(request){
+    let arr = ['row','val'];    // defines required keys
+    for(let key of arr){
+        if(request[key]==undefined)     // checks if the request doesn't contain this required key
+            return false;
+    }
+    return true;
+}
+/**
+ * Encapsulating `args` for calling GAS API
+ */
 function generatePayload(args){
-    const FUNC_NAME = 'setValue';
+    const FUNC_NAME = 'setStatusValue'; // defines GAS method name to call
     return JSON.stringify({
         "function": FUNC_NAME,
-        "parameters": [args.commentUrl, args.row, args.col, args.sheetName]
+        "parameters": [args.row, args.val]  // defines the arguments for the GAS method
     });
 }
 function getTokenStorage(callback){
@@ -62,18 +100,17 @@ function getTokenRemoteAndCache(callback){
 /**
  * args: object with commentUrl, row, col properties
  */
-function xhrWithAuth(args) {
+function xhrWithAuth(payload) {
     const TAG = 'xhrWithAuth';
-    var retry = true;    
-    const PAYLOAD = generatePayload(args);
+    var retry = true;
     // const API_KEY = 'AIzaSyBoOa9ile3fk8_dEo4DRMEzD2g4uRdvLI8';
     // const SCRIPT_ID = 'AKfycbxdp2tFOBq2XcKm_oZdj-oUbnn_SozhRwJeDE6mkgmjFtjevJ49iqoEVFVsmGhS7Pi4';
-    const DEPLOYMENT_ID = 'AKfycbx24LtTDu5pqt-fr4dvYvHZVmF02hcS2qNNY31tEIwgz7ECw0kjH7lmxwA0mv9wFQkc';
+    const DEPLOYMENT_ID = 'AKfycbzNq68LMVY9QLu2PDsTPKsZKDVxs3c74ydTyPZFuhu5gwdy1iN-tptJYuVwQpeIj7gtNg';
     const POST_URL = `https://script.googleapis.com/v1/scripts/${DEPLOYMENT_ID}:run`;
     
     
     /*** Get the access token and call the identity API ***/
-    function getToken() {
+    function xhr() {
         getTokenStorage(function(token){
             if(!token){
                 console.log(TAG,"No auth token saved in storage. Please click on the extension icon to do authorization first.");
@@ -85,7 +122,7 @@ function xhrWithAuth(args) {
                     'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json'
                 },
-                body: PAYLOAD
+                body: payload
             };
             fetch(POST_URL,fetchOptions).then(response=>{
                 responseHandler(response);
@@ -100,50 +137,36 @@ function xhrWithAuth(args) {
                 } else if(response.status==401 && retry){
                     console.log(TAG,`Request failed. Retrying with another token.`);
                     retry = false;
-                    chrome.identity.removeCachedAuthToken({ 'token': token }, function(){getTokenRemoteAndCache(getToken)});
+                    chrome.identity.removeCachedAuthToken({ 'token': token }, function(){getTokenRemoteAndCache(xhr)});
                 } else {
                     console.log(TAG,`Request failed with unknown solution.`);
                 }
             }
         });
     }
-    getToken();
+    xhr();
 }
   
-
-function isMessageMatchFormat(msg){
-    let arr = ['commentUrl','row','col'];
-    for(let key of arr){
-        if(msg[key]==undefined) return false;
-    }
-    return true;
-}
 /**
- * Called when receives message from a tab
+ * Called when receives message from a tab's content script
  */
 function onTabMessageListener(request, sender, sendResponse){
-    var TAG = "onTabMessageListener";
-    var tabId = sender.tab.id;
+    let TAG = "onTabMessageListener";
+    let tabId = sender.tab.id;
     console.log(TAG,`TabId ${tabId}: sent: `,request);  
     if(request==='ready'){
         //sends params to content script
-        var payload = JSON.stringify(getTabListInstance()[tabId]);
-        sendMessageContentScript(tabId,payload);
+        let payload = JSON.stringify(getTabListInstance()[tabId]);  // gets parameters assigned to this tab id
+        sendMessageContentScript(tabId,payload);    // sends the encoded param to content script in the tab
         console.log(TAG,'Sent message to content script at ',tabId,sender.tab.url,payload);
         return;
     }
-    if(!isMessageMatchFormat(request)) return;  // ensures sender's message contains our data
     let tabList = getTabListInstance();
-    if(tabList[tabId]==undefined) return;  // ensures this tab id is in the list
-    delete tabList[tabId];  // removes this tab id from the listed tabIds
-    let args = {
-        'commentUrl': request.commentUrl,
-        'row':request.row,
-        'col':request.col,
-        'sheetName':request.sheetName
-    };
-    // TODO: call GAS using Execution API here
-    xhrWithAuth(args);
+    if(!isMessageMatchFormat(request) || tabList[tabId]==undefined)
+        return;  // ensures sender's message contains our data
+    let payload = generatePayload(request); // creates payload for calling GAS API
+    console.log(TAG,`Tab #${tabId}: generated payload: `,payload);
+    xhrWithAuth(payload);
 }
 /**
  * Handles messages from popup.html
@@ -160,24 +183,21 @@ function onPopupMessageListener(request,sender,sendResponse){
             var msg = token?'Authorized':'Not authorized';
             sendResponse(msg);
         });
-        // TODO: check local auth token; status: no token, token expired, token ready
     } else if(request=='test'){
         console.log('Test started.');
-        xhrWithAuth({commentUrl:'done',col:5,row:15});
+        xhrWithAuth({row:5,val:'Hello from extension'});  // TODO: change params
     } 
     else{
         console.log("Request wrong format: "+request);
     }
 }
 // Events subscribing
-try{
-    chrome.tabs.onUpdated.addListener(onTabUpdatedListener);
-    chrome.runtime.onMessage.addListener(function(req,s,res){
-        if(s.tab) {     // checks if message comes from a tab
-            onTabMessageListener(req,s,res)
-        } else{
-            onPopupMessageListener(req,s,res);
-        }
-        return true;
-    });
-} catch(e){console.log(e);}
+chrome.tabs.onUpdated.addListener(onTabUpdatedListener);
+chrome.runtime.onMessage.addListener(function(req,s,res){
+    if(s.tab) {     // checks if message comes from a tab
+        onTabMessageListener(req,s,res)
+    } else{
+        onPopupMessageListener(req,s,res);
+    }
+    return true;
+});
--- content.js
+++ content.js
@@ -1,86 +1,131 @@
-// function injectJquery(){
-//     let scriptElm = document.createElement('script');
-//     scriptElm.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js';
-//     document.body.appendChild(scriptElm);
-// }
-function validateRequest(request){
-    return  typeof(request.timeout)!='number' 
-            || typeof(request.row)!='number' 
-            || typeof(request.col)!='number'
-            || typeof(request.comment)!='string'
-            || typeof(request.sheetName)!='string';
+// Supportive methods to defines document ready state
+function first2DegNode(elm){
+    // console.log(elm, elm.children, elm.children?elm.children.length:null);  // debug
+    if(elm.children.length==2)
+        return elm;
+    return first2DegNode(elm.parentElement)
 }
-function getViewportHeight(){
-    return document.documentElement.clientHeight;
+function findSvgContainer(root){
+    if(root.firstChild.tagName=='svg')
+        return root;
+    return findSvgContainer(root.firstChild)
 }
-function getDocumentHeight(){
-    let body = document.body;
-    let html = document.documentElement;
-    return Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+function getCommentFormElement(){
+    return document.querySelector('form:last-child');
 }
-function getCommentElements(){
-    let selector = '#header-author > yt-formatted-string > a';  // defines the selector
-    return document.querySelectorAll(selector);
+function isDocumentReady(){
+    let formElm = getCommentFormElement();
+    if(!formElm){   // checks if not foudn formElm
+        return false;
+    }
+    let container = findSvgContainer(first2DegNode(formElm.parentElement));
+    return container.children.length==2;    // returns if the container has 2 children
 }
-function stimulateKeyboardInput(target){
-	let options = {code:"Space",key:" ",keyCode:32};
-    let optionsInput = {data:' '};
-        let events = [new FocusEvent('focus'), new KeyboardEvent('keydown',options), new KeyboardEvent('keypress',options), new InputEvent('input',optionsInput), new KeyboardEvent('keyup',options)];
+// Supportive methods for making the comment
+function stimulateKeyboardInput(target, options){
+    // 	let options = isSpaceKey?{code:"Space",key:" ",keyCode:32,bubbles:true, cancelBubble:false}
+    //                             :{code:"Enter",key:"Enter",keyCode:13,bubbles:true, cancelBubble:false};
+    let payload = {
+        bubbles:true,
+        cancelBubble:false,
+        cancelable:true,
+        composed:true,
+        isTrusted:true,
+        view:window
+    };
+    options = {...options,...payload};
+    let optionsInput = {data:options.key,...payload};
+    console.log('keyboard:',options,'input:',optionsInput);   //debugging    
+    target.focus(); // backup for failing focus event
+    target.click(); // backup for failing focus event
+    let events = [new FocusEvent('focus'), new KeyboardEvent('keydown',options), new KeyboardEvent('keypress',options), new InputEvent('input',optionsInput), new KeyboardEvent('keyup',options)];
+    // let events = [new FocusEvent('focus'), new KeyboardEvent('keydown',options), new KeyboardEvent('keypress',options), new KeyboardEvent('keyup',options)];
     events.forEach(e=>target.dispatchEvent(e));  
     console.log('done');//debugging
 }
-function solve(root, cb){
-	let queue = [];
-    queue.push(root);
-    while(queue.length>0){
-        let node = queue.shift();	// pop head
-        cb(node);
-        let children = node.children;
-        for(let i = 0; i<children.length; ++i){
-            queue.push(children[i]);
-        }
-    }
+/**
+ * Shortcut to gets common key options
+ */
+function getKeyOptions(isSpace=true){
+    return isSpace?{key:' ',keyCode:32,code:'Space'}
+                    :{key:'Enter',keyCode:13,code:'Enter'};
 }
+// function solve(root, cb){
+// 	let queue = [];
+//     queue.push(root);
+//     while(queue.length>0){
+//         let node = queue.shift();	// pop head
+//         cb(node);
+//         let children = node.children;
+//         for(let i = 0; i<children.length; ++i){
+//             queue.push(children[i]);
+//         }
+//     }
+// }
+function getDocumentHeight(){
+    var body = document.body, html = document.documentElement;
+    var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+    return height;
+}
+/**
+ * Gets all comment elements in the page
+ */
+function getCommentElements(){
+    let ulists = document.querySelectorAll('div[data-visualcompletion="ignore-dynamic"] ul');
+    console.log('ulists:',ulists, ulists.length==1);  // debugging
+    if(ulists.length==1)    // checks if there are no other comments
+        return [];
+    return ulists[0].children;  // returns list of comment options elements (for: like, url,..)
+}
+function getCmtDiv(){
+    return document.querySelector('form:last-child div[contenteditable="true"]');     // gets the main comment element
+}
+function getSpanIn(cmtDiv){
+    return cmtDiv.querySelector('span[data-text="true"]');
+}
+/**
+ * Main method to make the comment on script injected
+ */
 function makeComment(comment, callback){
     const TAG = 'makeComment';
     let interval = 1000; // sets a small interval
     console.log(TAG,'Started');
     let t = setInterval(function(){
         console.log(TAG,'checking webpage loaded..');
-        if(getViewportHeight()/getDocumentHeight()>0.7) return; // checks if youtube has not finished loading
+        // if(!isDocumentReady()){ // checks if document not ready to make comment
+        //     return;
+        // }
+        let cmtDiv = getCmtDiv();
+        if(!cmtDiv) // checks if youtube has not finished loading
+            return; 
         clearInterval(t);   // stops checking if webpage is loaded
         let height = getDocumentHeight();   // gets current full document height
-        scrollBy(0,getViewportHeight()/2);  // scrolls to half viewport height
+        scrollBy(0,height);  // scrolls to bottom of page
         
-        let t2 = setInterval(function(){
-            console.log(TAG,'checking comments loaded..');
-            // if(height==getDocumentHeight()) return; // check if document height not changed
-            
-            let commentBox = document.querySelector('#placeholder-area');    // gets the clickable comment box
-            if(commentBox==null) {
-                scrollBy(0,getViewportHeight()/2);
-                height = getDocumentHeight();   // sets the height to current document height
+        console.log(TAG,'making comment..');
+        // let cmtDiv = getCmtDiv();
+        stimulateKeyboardInput(cmtDiv,{key:' ',keyCode:32,code:'Space'});
+        let span = getSpanIn(cmtDiv);
+        span.innerHTML = comment;
+        stimulateKeyboardInput(cmtDiv,{key:' ',keyCode:32,code:'Space'});
+
+        let cmtElmsCount = getCommentElements().length; // counts the comment elements
+        stimulateKeyboardInput(getCmtDiv(),getKeyOptions(false));   // makes comment
+        var t3 = setInterval(function(){    // keeps trying this function
+            let currElms = getCommentElements();    // gets list of comment elements
+            console.log(cmtElmsCount,'->',currElms.length);   // debugging
+            if(currElms.length== cmtElmsCount)   // checks if no new comment has been made
+                return;
+            var cmtElm = currElms[currElms.length-1]; // gets the last comment element
+            console.log('cmtElm:',cmtElm);  // debugging
+            let cmtUrl = cmtElm.querySelector('ul:last-child li:last-child a');  // gets the comment url element
+            if(!cmtUrl){    // checks if cmtUrl has not been found
                 return;
             }
-            
-            clearInterval(t2);
-            console.log(TAG,'making comment..');
-            commentBox.click();     // clicks onto the box so DOM renders the input area
-            let commentInput = document.querySelector('#contenteditable-root');     // gets youtube comment input element
-            commentInput.innerText = comment;   // sets the comment
-            let root = document.querySelector('#creation-box');
-            solve(root,stimulateKeyboardInput);     // sends key input to all these elements
-            let commentButton = document.querySelector('#submit-button');
-            console.log(TAG,'comment ready');
-            commentButton.click();  // clicks comment button
-            let urls = getCommentElements();
-            let t3 = setInterval(function(){
-                let currUrls = getCommentElements();
-                if(currUrls.length<= urls.length) return;
-                
-                clearInterval(t3);
-                callback(currUrls[0].href);
-            },interval);
+            cmtUrl = cmtUrl.href;   // gets the url of this anchor tag
+            clearInterval(t3);  // stops trying
+            console.log('cmtUrl:',cmtUrl);   // debugging
+            callback(cmtUrl);  // calls the callback method with the comment url
         },interval);
     },interval);
 }
@@ -92,33 +137,34 @@ function sendMessageExtension(message,callback){
         callback();
     }
 }
+/**
+ * Main procedure after received arguments
+ */
 function main(args){
-    // calls makeComment after `timeout` interval
     const TAG = 'main';
-    var timeout = args.timeout;
-    console.log(TAG, 'Started. Waiting until comment process: ',timeout+'ms');
-    setTimeout(function(){
-        makeComment(args.comment, function(commentUrl){
-            console.log(TAG, 'Got comment URL: ',commentUrl);
-            sendMessageExtension({commentUrl,row:args.row,col:args.col,sheetName:args.sheetName}, function(){
-                window.close();
-            });    // sends the url href to extension side
+    if(args.type=='comment'){   // checks if the args is for making comment
+        let {comment, row} = args.params;   // extracts comment and row value from arguments
+        makeComment(comment, function(val){
+            let msgPayload = JSON.stringify({row,val}); // creates correct format object to send to the extension
+            // sendMessageExtension(msgPayload, window.close); // sends message then close
+            sendMessageExtension(msgPayload); // sends message to extension
         });
-    },timeout);
-}
-// sends message to extension to get parameters
-sendMessageExtension('ready');
-// listens for messages
-chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
-    const TAG = 'onMessage';
-    request = JSON.parse(request);
-    console.log(TAG, 'Received message: ',request, 'Sender: ',sender);
-    if(!sender.tab){    // checks if the message comes from the extension
-        if(!validateRequest(request)){
-            console.error({message:'Parameters invalid.',request:request});
-        } else{
-            console.log(TAG,'Received parameters: ',request);
-            main(request);
-        }
+    } else{ // checks if the args is for deleting comment
+        // TODO
     }
-})
+}
+
+if(!globalThis.existed){    // checks if the content script is injected before
+    globalThis.existed = true;  // helper variable showing the content script is injected before
+    // sends message to extension to get parameters
+    sendMessageExtension('ready');
+    // listens for messages
+    chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+        const TAG = 'onMessage';
+        request = JSON.parse(request);
+        console.log(TAG, 'Received message: ',request, 'Sender: ',sender);
+        main(request);  // runs main procedure
+    })
+} else{
+    console.log('Content script already injected. Skipping initialization');    // debugging
+}
